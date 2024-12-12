1da20d774acd3c0db08e99529049063bdbc172a0
full-stack-ai <fullstackinfo.ai@gmail.com>
2024-12-11 22:04:02 -0700

    add setup.py to setup the package as

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ai-chatbot",  # Replace with your package name
+    version="0.1.0",           # Set the initial version
+    description="A brief description of your package",
+    author="Hadi",
+    author_email="fullstackailab@gmail.com",
+    url="https://github.com/full-stack-ai/ai-chatbot-backend-service",  # Update with your repo URL
+    packages=find_packages(),  # Automatically finds all sub-packages
+    install_requires=[
+    ],
+    python_requires=">=3.10",  # Specify the Python versions your package supports
+)
